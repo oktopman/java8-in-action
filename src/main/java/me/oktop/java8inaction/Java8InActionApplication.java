5f9ce6208c3792920ddb19a8914171c309ffc36f
oktopman <colaws3@gmail.com>
2020-09-15 18:42:35 +0900

    initialize commit

---
+++ src/main/java/me/oktop/java8inaction/Java8InActionApplication.java
@@ -0,0 +1,13 @@
+package me.oktop.java8inaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java8InActionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Java8InActionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/oktop/java8inaction/Java8InActionApplicationTests.java
@@ -0,0 +1,13 @@
+package me.oktop.java8inaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java8InActionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
